8dc8b5d92f4c1a48e42cebc3853d7cedf08c8bcb
yaoyu bao <yaobao5354@gmail.com>
2020-03-20 17:35:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 require 'pry'
 class App < Sinatra::Base
   get '/reverse' do
-    original_string = params["string"]
+    original_string = params[:string]
     @reversed_string = original_string.reverse
     erb :reverse
   end
